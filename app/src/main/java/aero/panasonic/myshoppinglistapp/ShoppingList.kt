04f42aa7f3883c1c4642640ddee331253ed87c5c
David Park <yedarm.park@panasonic.aero>
2024-09-05 09:46:09 -0700

    ShoppingListItem composable

--- app/src/main/java/aero/panasonic/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/aero/panasonic/myshoppinglistapp/ShoppingList.kt
@@ -1,6 +1,9 @@
 package aero.panasonic.myshoppinglistapp
 
 import android.content.pm.ModuleInfo
+import android.view.Display.Mode
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,8 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -35,7 +45,7 @@ fun ShoppingListApp(innerPadding: PaddingValues) {
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
     var itemName by remember { mutableStateOf("") }
-    var itemQuantity by remember { mutableStateOf("1") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -55,7 +65,7 @@ fun ShoppingListApp(innerPadding: PaddingValues) {
                 .padding(16.dp)
         ) {
             items(sItems) {
-
+                ShoppingListItem(item = it, onEditClick = {}, onDeleteClick = {})
             }
         }
     }
@@ -78,7 +88,7 @@ fun ShoppingListApp(innerPadding: PaddingValues) {
                             sItems = sItems + newItem
                             showDialog = false
                             itemName = ""
-                            itemQuantity = "1"
+                            itemQuantity = ""
                         }
                     }) {
                         Text(text = "Add")
@@ -109,5 +119,46 @@ fun ShoppingListApp(innerPadding: PaddingValues) {
                 )
             } }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+     onEditClick: () -> Unit,
+     onDeleteClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = item.name, modifier = Modifier.padding(8.dp))
+            Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        }
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = onEditClick,
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(
+                onClick = onDeleteClick,
+            ) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
     }
 }
